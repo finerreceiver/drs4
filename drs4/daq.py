9b16bd1e839ce51a87182cc62649e9c1d5fbbed9
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2024-10-20 08:43:58 +0000

    #3 Add data acquisition (daq) module

---
+++ drs4/daq.py
@@ -0,0 +1,91 @@
+__all__ = ["dump"]
+
+
+# standard library
+from logging import getLogger
+from multiprocessing.synchronize import Event
+from pathlib import Path
+from socket import (
+    IP_ADD_MEMBERSHIP,
+    IPPROTO_IP,
+    SO_REUSEADDR,
+    SOCK_DGRAM,
+    SOL_SOCKET,
+    inet_aton,
+    socket,
+)
+from tqdm import tqdm
+from typing import Optional, Union
+
+
+# dependencies
+from .specs import VDIF_FRAME_BYTES
+
+
+# constants
+GROUP = "239.0.0.1"
+LOGGER = getLogger(__name__)
+
+
+def dump(
+    vdif: Union[Path, str],
+    addr: str,
+    port: int,
+    /,
+    *,
+    group: str = GROUP,
+    cancel: Optional[Event] = None,
+    timeout: Optional[float] = None,
+    progress: bool = False,
+    overwrite: bool = False,
+) -> None:
+    """Receive and dump DRS4 data per input into a VDIF file.
+
+    Args:
+        vdif: Path of the output VDIF file.
+        addr: Destination IP address.
+        port: Destination port number.
+        group: Multicast group IP address.
+        cancel: Event object to cancel dumping.
+        timeout: Timeout period in units of seconds.
+        progress: Whether to show the progress bar on screen.
+        overwrite: Whether to overwrite the existing VDIF file.
+
+    Raises:
+        FileExistsError: Raised if overwrite is not allowed
+            and the output VDIF file already exists.
+        TimeoutError: Raised if no DRS4 data (i.e. VDIF frame)
+            is received for the timeout period.
+
+    """
+    if not overwrite and Path(vdif).exists():
+        raise FileExistsError(vdif)
+
+    prefix = f"[{addr=}, {port=}]"
+    mreq = inet_aton(group) + inet_aton(addr)
+
+    with (
+        open(vdif, "wb") as file,
+        socket(type=SOCK_DGRAM) as sock,
+        tqdm(desc=prefix, disable=not progress, unit="byte") as bar,
+    ):
+        # create socket
+        sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        sock.bind(("", port))
+        sock.setsockopt(IPPROTO_IP, IP_ADD_MEMBERSHIP, mreq)
+        sock.settimeout(timeout)
+
+        # start dumping
+        LOGGER.info(f"{prefix} Start dumping data.")
+
+        while cancel is None or not cancel.is_set():
+            frame, _ = sock.recvfrom(VDIF_FRAME_BYTES)
+
+            if len(frame) == VDIF_FRAME_BYTES:
+                file.write(frame)
+                bar.update(VDIF_FRAME_BYTES)
+            else:
+                LOGGER.warning(f"{prefix} Truncated frame.")
+
+        # finish dumping
+        LOGGER.info(f"{prefix} Finish dumping data.")
