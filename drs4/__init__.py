643bca2542cbdaf39bf223c47123b1843a289282
Akio Taniguchi <13254278+astropenguin@users.noreply.github.com>
2025-02-05 02:29:24 +0000

    #3 Add VDIF reading (vdif) module

--- drs4/__init__.py
+++ drs4/__init__.py
@@ -1,6 +1,7 @@
-__all__ = ["specs"]
+__all__ = ["specs", "vdif"]
 __version__ = "0.1.0"
 
 
 # submodules
 from . import specs
+from . import vdif
---
+++ drs4/vdif.py
@@ -0,0 +1,157 @@
+__all__ = ["open_vdif"]
+
+
+# standard library
+from dataclasses import dataclass
+from os import PathLike
+from typing import Literal as L, Union
+
+
+# dependencies
+import numpy as np
+import xarray as xr
+from numpy.typing import NDArray
+from xarray_dataclasses import AsDataArray, Attr, Coordof, Data, Dataof
+
+
+# type hints
+IntegTime = L[100, 200, 500, 1000]  # ms
+StrPath = Union[PathLike[str], str]
+
+
+# constants
+CHAN_FIRST_HALF = np.arange(0, 256)
+CHAN_SECOND_HALF = np.arange(256, 512)
+FREQ_FIRST_HALF = 0.02 * CHAN_FIRST_HALF  # GHz
+FREQ_SECOND_HALF = 0.02 * CHAN_SECOND_HALF  # GHz
+REF_EPOCH_ORIGIN = np.datetime64("2000", "Y")  # UTC
+REF_EPOCH_UNIT = np.timedelta64(6, "M")
+
+
+# data classes
+@dataclass
+class Time:
+    data: Data[L["time"], L["M8[ns]"]]
+    long_name: Attr[str] = "Measured time in UTC"
+
+
+@dataclass
+class Freq:
+    data: Data[L["freq"], np.float64]
+    long_name: Attr[str] = "Measured frequency"
+    units: Attr[str] = "GHz"
+
+
+@dataclass
+class Chan:
+    data: Data[L["freq"], np.int64]
+    long_name: Attr[str] = "Channel number"
+
+
+@dataclass
+class Auto:
+    data: Data[tuple[L["time"], L["freq"]], np.float64]
+    long_name: Attr[str] = "Auto-correlation spectra"
+    units: Attr[str] = "Arbitrary unit"
+
+
+@dataclass
+class VDIF(AsDataArray):
+    """Data specifications of VDIF."""
+
+    # dims
+    time: Coordof[Time]
+    """Measured time in UTC."""
+
+    freq: Coordof[Freq]
+    """Measured frequency in GHz."""
+
+    # coords
+    chan: Coordof[Chan]
+    """Channel number (0-511)."""
+
+    # vars
+    auto: Dataof[Auto]
+    """Auto-correlation spectra."""
+
+    # attrs
+    integ_time: Attr[IntegTime]
+    """Spectral integration time in ms (100|200|500|1000)."""
+
+
+@dataclass(frozen=True)
+class Word:
+    """VDIF header word parser."""
+
+    data: NDArray[np.int_]
+    """VDIF header word as a 1D integer array."""
+
+    def __getitem__(self, index: slice, /) -> NDArray[np.int_]:
+        """Slice the VDIF header word."""
+        start, stop = index.start, index.stop
+        return (self.data >> start) & ((1 << stop - start) - 1)
+
+
+def open_vdif(
+    vdif: StrPath,
+    /,
+    *,
+    integ_time: IntegTime = 100,
+) -> xr.DataArray:
+    """Open a VDIF file as a DataArray.
+
+    Args:
+        vdif: Path of input VDIF file.
+        integ_time: Spectral integration time in ms.
+
+    Returns:
+        DataArray of the input VDIF file.
+
+    """
+    with open(vdif, "rb") as f:
+        data = np.frombuffer(
+            f.read(),
+            dtype=[
+                ("word_0", "u4"),
+                ("word_1", "u4"),
+                ("word_2", "u4"),
+                ("word_3", "u4"),
+                ("word_4", "u4"),
+                ("word_5", "u4"),
+                ("word_6", "u4"),
+                ("word_7", "u4"),
+                ("data", ("f4", 256)),
+            ],
+        )
+        word_0 = Word(data["word_0"])
+        word_1 = Word(data["word_1"])
+
+    time = (
+        REF_EPOCH_ORIGIN
+        + REF_EPOCH_UNIT * word_1[24:30]
+        + np.timedelta64(1, "s") * word_0[0:30]
+        + np.timedelta64(integ_time, "ms") * (word_1[0:24] // 2)
+    )
+    is_first_half = word_1[0:24] % 2 == 0
+    is_second_half = word_1[0:24] % 2 == 1
+
+    return xr.concat(
+        (
+            VDIF.new(
+                time=time[is_first_half],
+                freq=FREQ_FIRST_HALF,
+                chan=CHAN_FIRST_HALF,
+                auto=data["data"][is_first_half],
+                integ_time=integ_time,
+            ),
+            VDIF.new(
+                time=time[is_second_half],
+                freq=FREQ_SECOND_HALF,
+                chan=CHAN_SECOND_HALF,
+                auto=data["data"][is_second_half],
+                integ_time=integ_time,
+            ),
+        ),
+        dim="freq",
+        join="inner",
+    )
