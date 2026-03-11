af54ce378219fb022d57beaffc7399c955c5439a
Akio Taniguchi <13254278+astropenguin@users.noreply.github.com>
2026-03-11 19:05:15 +0900

    #77 Fix query detection of autorecv option

--- drs4/ctrl/scpi.py
+++ drs4/ctrl/scpi.py
@@ -115,7 +115,7 @@ def send_commands(
         port: Port of the server.
         timeout: Timeout value in units of seconds.
         encoding: Encoding format for the commands.
-        autorecv: If True and a command ends with '?',
+        autorecv: If True and a command contains '?',
             receive a message and record it to a logger.
         bufsize: Maximum byte size for receiving a message.
 
@@ -151,7 +151,7 @@ def send_commands(
 
             sock.send(command.strip(), encoding=encoding)
 
-            if autorecv and command.endswith("?"):
+            if autorecv and "?" in command:
                 messages.append(sock.recv(bufsize))
 
         return tuple(messages)
@@ -175,7 +175,7 @@ def send_commands_in(
         port: Port of the server.
         timeout: Timeout value in units of seconds.
         encoding: Encoding format for the commands.
-        autorecv: If True and a command ends with '?',
+        autorecv: If True and a command contains '?',
             receive a message and record it to a logger.
         bufsize: Maximum byte size for receiving a message.
 
